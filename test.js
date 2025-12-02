4c9e424e744d8958dbe4ea0d9deee7e71efd0e75
Abhishek93248 <80088028+Abhishek93248@users.noreply.github.com>
2025-12-02 16:36:22 +0530

    doneee

--- test.js
+++ test.js
@@ -2,13 +2,23 @@ import puppeteer from "puppeteer";
 import percySnapshot from "@percy/puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--disable-software-rasterizer"
+    ]
+  });
+
   const page = await browser.newPage();
 
-  // URL through BrowserStack Local tunnel
-  await page.goto("http://localhost:3000");
+  await page.goto("http://localhost:3000", {
+    waitUntil: "networkidle0"
+  });
 
-  // Take visual snapshot
   await percySnapshot(page, "Homepage Snapshot");
 
   await browser.close();
